90770845e5c9b6c78e6e82b63a2a55efa81be3aa
ZinixZay <romadesl21@gmail.com>
2022-05-17 19:01:07 +0300

    init

---
+++ db.py
@@ -0,0 +1,65 @@
+import psycopg2
+import datetime
+from config import *
+
+sql = psycopg2.connect(
+    database=db_name,
+    user=user_name,
+    password=password,
+    host=host,
+    port=port
+)
+
+cur = sql.cursor()
+
+
+def execute(com: str):
+    cur.execute(com)
+    sql.commit()
+
+
+def catch_request(request: dict):
+    nickname = request['nickname']
+    name = request['name']
+    answer = dict()
+    if request['status'] == 'host':
+        answer = create_lobby(name, nickname, request['status'])
+    elif request['status'] == 'connect':
+        answer = join_lobby(name, nickname, request['status'])
+    return answer
+
+
+def create_lobby(name: str = 'def', nickname: str = "def", role: str = 'def'):
+    try:
+        command = f"CREATE TABLE {name}" \
+                  f"(" \
+                  f"Id SERIAL PRIMARY KEY," \
+                  f"name CHARACTER VARYING(30)," \
+                  f"role CHARACTER VARYING(20)," \
+                  f"server_role CHARACTER VARYING(10)," \
+                  f"location CHARACTER VARYING(30)," \
+                  f"status BOOLEAN DEFAULT (false))"
+        execute(command)
+    except Exception:
+        content = 'Упс! Произошла ошибка. Скорее всего лобби с таким названием уже существует'
+        return {'status': 'error', 'content': content}
+    command = f"INSERT INTO {name} (name, server_role) VALUES ('{nickname}', '{role}')"
+    execute(command)
+    return {'status': 'ok'}
+
+
+def join_lobby(name: str = 'def', nickname: str = 'def', role: str = 'def'):
+    try:
+        nicknames = list()
+        cur.execute(f"SELECT name FROM {name}")
+        for nick in cur.fetchall():
+            nicknames.append(nick[0])
+    except Exception:
+        content = 'Упс! Произошла ошибка. Скорее всего лобби с таким названием не существует'
+        return {'status': 'error', 'content': content}
+    if nickname in nicknames:
+        content = 'Упс! Произошла ошибка. Скорее всего игрок с таким ником уже вошел в лобби'
+        return {'status': 'error', 'content': content}
+    command = f"INSERT INTO {name} (name, server_role) VALUES ('{nickname}', '{role}')"
+    execute(command)
+    return {'status': 'ok'}
---
+++ main.py
@@ -0,0 +1,78 @@
+import sys
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.uic import loadUi
+from PyQt5.QtWidgets import QDialog, QApplication, QMainWindow, QMessageBox
+from PyQt5.QtGui import QFontDatabase, QFont
+from db import *
+from errors import error_codes
+
+
+def showerror(content: str):
+    msg = QMessageBox()
+    msg.setIcon(QMessageBox.Critical)
+    msg.setText(f'Error code = {error_codes[content]}')
+    msg.setInformativeText(content)
+    msg.setWindowTitle("Error")
+    msg.exec_()
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        loadUi("screens/main.ui", self)
+        self.hostButton.clicked.connect(self.host_clicked)
+        self.connectButton.clicked.connect(self.connect_clicked)
+
+    def host_clicked(self):
+        global call_status, nick
+        nick = self.nameLine.text()
+        call_status = 'host'
+        widget.setCurrentIndex(widget.currentIndex() + 1)
+
+    def connect_clicked(self):
+        global call_status, nick
+        nick = self.nameLine.text()
+        call_status = 'connect'
+        widget.setCurrentIndex(widget.currentIndex() + 1)
+
+
+class Servername(QMainWindow):
+    def __init__(self):
+        global call_status
+        super(Servername, self).__init__()
+        loadUi("screens/server.ui", self)
+        self.backButton.clicked.connect(self.back_clicked)
+        self.doneButton.clicked.connect(self.done_clicked)
+
+    def done_clicked(self):
+        name = self.servernameLine.text().replace(' ', '_')
+        request = {'name': name,
+                   'nickname': nick,
+                   'status': call_status}
+        answer = catch_request(request)
+        if answer['status'] == 'error':
+            showerror(answer['content'])
+
+    @staticmethod
+    def back_clicked():
+        widget.setCurrentIndex(widget.currentIndex() - 1)
+
+
+app = QApplication(sys.argv)
+id = QFontDatabase.addApplicationFont(':fonts/20665.ttf>')
+widget = QtWidgets.QStackedWidget()
+mainWindow = MainWindow()
+serverName = Servername()
+font = QFont('somefont')
+widget.setFont(font)
+widget.setWindowTitle('Spy')
+widget.addWidget(mainWindow)
+widget.addWidget(serverName)
+widget.setFixedSize(800, 600)
+widget.show()
+
+
+try:
+    sys.exit(app.exec_())
+except:
+    print('Exiting')
